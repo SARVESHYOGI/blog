115cc56770cc9cf5bdc7b689d1a8095cd7e5b61f
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2024-09-17 19:45:28 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:8000/api/v1" });
-// const axiosInstance = axios.create({ baseURL: "https://blog-w6j9.onrender.com/api/v1" });
+// const axiosInstance = axios.create({ baseURL: "http://localhost:8000/api/v1" });
+const axiosInstance = axios.create({ baseURL: "https://blog-w6j9.onrender.com/api/v1" });
 
 axiosInstance.interceptors.request.use((req) => {
   const stringifyBlogData = window.localStorage.getItem("blogData");
